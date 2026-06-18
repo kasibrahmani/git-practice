a5a47a81f97f06dea95145e8c340ff43d5782c32
Brahmani <kasibrahmani@gmail.com>
2026-06-18 13:11:35 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const mimeTypes = {
+  ".html": "text/html; charset=utf-8",
+  ".css": "text/css; charset=utf-8",
+  ".js": "text/javascript; charset=utf-8",
+  ".json": "application/json; charset=utf-8",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".svg": "image/svg+xml"
+};
+
+const server = http.createServer((req, res) => {
+  const requestedPath = req.url === "/" ? "/index.html" : req.url;
+  const filePath = path.join(__dirname, "public", requestedPath);
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("404 - Page not found");
+      return;
+    }
+
+    const ext = path.extname(filePath);
+    res.writeHead(200, { "Content-Type": mimeTypes[ext] || "application/octet-stream" });
+    res.end(content);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
